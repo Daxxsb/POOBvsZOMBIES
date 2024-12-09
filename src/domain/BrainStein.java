96bd00c46625df28ffdb84b239bde0bfc6ec22d2
Carlos-Avellaneda <carlos.avellaneda-f@mail.escuelaing.edu.co>
2024-12-09 12:46:34 -0500

    Agregue el BrainStein pero toca modificar la clase abstracta de zombie, para hacer el BrainStein copie el girasol y lo "adapte" a zombie

---
+++ src/domain/BrainStein.java
@@ -0,0 +1,45 @@
+package domain;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class BrainStein extends Zombie {
+    private int cerebrosGenerados;
+    private Timer timer;
+
+    public BrainStein(int vida, String tipo, int[] xy) {
+        vida = 0;
+        tipo = "si";
+        cerCost = 50;
+        cerebrosGenerados = 0;
+        startGeneratingCerebros();
+    }
+
+    private void startGeneratingCerebros() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                cerebrosGenerados += 25;
+                System.out.println("Cerebros generados: " + cerebrosGenerados);
+            }
+        }, 0, 20000);
+
+    }
+
+    public int getTotalCerebros() {
+        return cerebrosGenerados;
+    }
+
+    public void resetCerebrosGenerados() {
+        cerebrosGenerados = 0;
+    }
+
+    public void stopGeneratingCerebros() {
+        if (timer != null) {
+            timer.cancel();
+            System.out.println("Generación de Cerebros detenida.");
+        }
+    }
+}
+
